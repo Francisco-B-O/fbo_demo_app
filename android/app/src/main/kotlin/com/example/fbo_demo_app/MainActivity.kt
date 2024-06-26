012e78cd7e35d2356cd68faaf69ded2596ef61e9
FranciscoAgora1 <agora1@informatica.iesvalledeljerteplasencia.es>
2024-06-26 12:20:46 +0200

    feat: :rocket: gh pages web

--- android/app/src/main/kotlin/com/example/fbo_demo_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fbo_demo_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/presentation/providers/counter_provider.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final counterProvider = StateProvider<int>((ref) => 5);
